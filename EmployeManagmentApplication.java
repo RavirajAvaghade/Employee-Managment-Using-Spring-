10b0f0427d5e00755178a02565ce1d258b339d6b
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-05-24 11:08:43 +0530

    Add files via upload

---
+++ EmployeManagmentApplication.java
@@ -0,0 +1,16 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeManagmentApplication 
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(EmployeManagmentApplication.class, args);
+		System.out.println("Application Started ");
+	}
+
+}
